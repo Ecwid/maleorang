cceb5c1d561469d13bda95b98cb4985c3697b504
Alexandre Pocheau <apocheau.ext@galerieslafayette.com>
2016-10-05 14:12:32 +0200

    use spy

--- src/test/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportsTest.kt
+++ src/test/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportsTest.kt
@@ -6,7 +6,7 @@ import com.ecwid.maleorang.connector.HttpClientConnector
 import com.ecwid.maleorang.method.v3_0.lists.GetAbuseReportMethod
 import com.ecwid.maleorang.method.v3_0.lists.GetAbuseReportsMethod
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
 import org.testng.Assert
 import org.testng.annotations.Parameters
 import org.testng.annotations.Test
@@ -14,6 +14,10 @@ import org.testng.annotations.Test
 class AbuseReportsTest
 @Parameters("mailchimp.test.apikey", "mailchimp.test.listid", "mailchimp.test.abusereportid")
 constructor(private val apiKey: String, private val listId: String, private val abuseReportId: String) {
+
+    var json = "{\"id\": 42,\"campaign_id\": \"839488a60b\",\"list_id\": \"1a2df69511\",\"email_id\": \"62eeb292278cc15f5817cb78f7790b08\",\"email_address\": \"urist.mcvankab@freddiesjokes.com\",\"date\": \"2015-07-15T19:19:31+00:00\",\"links\": [{\"rel\": \"self\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports/42\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Instance.json\" },{ \"rel\": \"parent\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Collection.json\",\"schema\": \"https://api.mailchimp.com/schema/3.0/CollectionLinks/Lists/Abuse.json\" }]}"
+    var request = Connector.Request("", "", "", "", "")
+    var response = Connector.Response(200, "", json)
 
     @Test
     fun testGetAbuseReports() {
@@ -30,10 +34,12 @@ constructor(private val apiKey: String, private val listId: String, private val 
     @Test
     fun testGetAbuseReport() {
 
-        var json = "{\"id\": 42,\"campaign_id\": \"839488a60b\",\"list_id\": \"1a2df69511\",\"email_id\": \"62eeb292278cc15f5817cb78f7790b08\",\"email_address\": \"urist.mcvankab@freddiesjokes.com\",\"date\": \"2015-07-15T19:19:31+00:00\",\"links\": [{\"rel\": \"self\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports/42\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Instance.json\" },{ \"rel\": \"parent\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Collection.json\",\"schema\": \"https://api.mailchimp.com/schema/3.0/CollectionLinks/Lists/Abuse.json\" }]}"
-        var response = Connector.Response(200, "", json)
-        val connector = `mock`(HttpClientConnector::class.java)
-        `when`(connector.call(Connector.Request("", "", "", "", ""))).thenReturn(response)
+        var connector = HttpClientConnector()
+        var spiedConnector = spy(connector)
+//        when(spiedConnector.call()).thenReturn(response);
+
+//        val connector = `mock`(HttpClientConnector::class.java)
+        `when`(spiedConnector.call(request)).thenReturn(response)
 
         MailchimpClient(apiKey).use { client ->
             client.execute(GetAbuseReportMethod(listId, abuseReportId)).apply {
