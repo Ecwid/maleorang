bea5f4177c22a915bb6435bdcc2692be3484cc84
Alexandre POCHEAU <apocheau@ippon.fr>
2016-10-13 22:23:56 +0200

    Fix AbuseReport

--- src/main/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportInfo.kt
+++ src/main/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportInfo.kt
@@ -2,6 +2,7 @@ package com.ecwid.maleorang.method.v3_0.lists
 
 import com.ecwid.maleorang.MailchimpObject
 import com.ecwid.maleorang.annotation.Field
+import java.util.*
 
 /**
  * Created by apocheau on 27/08/16.
@@ -30,7 +31,7 @@ class   AbuseReportInfo : MailchimpObject() {
 
     @JvmField
     @Field
-    var merge_fields: Object? = null
+    var merge_fields: MailchimpObject? = null
 
     @JvmField
     @Field
@@ -38,54 +39,9 @@ class   AbuseReportInfo : MailchimpObject() {
 
     @JvmField
     @Field
-    var date: String? = null
+    var date: Date? = null
 
     @JvmField
     @Field
     var _links: List<LinkInfo>? = null
-
-    fun setId(id: String): AbuseReportInfo{
-        this.id = id
-        return this
-    }
-
-    fun setCampaign_id(campaign_id: String): AbuseReportInfo{
-        this.campaign_id = campaign_id
-        return this
-    }
-
-    fun setListId(list_id: String): AbuseReportInfo{
-        this.list_id = list_id
-        return this
-    }
-
-    fun setEmailId(email_id: String): AbuseReportInfo{
-        this.email_id = email_id
-        return this
-    }
-
-    fun setEmailAddress(email_address: String): AbuseReportInfo{
-        this.email_address = email_address
-        return this
-    }
-
-    fun setMergeFields(merge_fields: Object): AbuseReportInfo{
-        this.merge_fields = merge_fields
-        return this
-    }
-
-    fun setVip(vip: Boolean): AbuseReportInfo{
-        this.vip = vip
-        return this
-    }
-
-    fun setDate(date: String): AbuseReportInfo{
-        this.date = date
-        return this
-    }
-
-    fun setLinks(_links: List<LinkInfo>): AbuseReportInfo{
-        this._links = _links
-        return this
-    }
 }
--- src/test/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportsTest.kt
+++ src/test/java/com/ecwid/maleorang/method/v3_0/lists/abuse_reports/AbuseReportsTest.kt
@@ -1,20 +1,18 @@
 package com.ecwid.maleorang.method.v3_0.lists.members
 
 import com.ecwid.maleorang.MailchimpClient
+import com.ecwid.maleorang.connector.Connector
+import com.ecwid.maleorang.method.v3_0.lists.GetAbuseReportMethod
 import com.ecwid.maleorang.method.v3_0.lists.GetAbuseReportsMethod
 import org.testng.Assert
 import org.testng.annotations.Parameters
 import org.testng.annotations.Test
+import java.util.logging.Logger
 
 class AbuseReportsTest
-@Parameters("mailchimp.test.apikey", "mailchimp.test.listid", "mailchimp.test.abusereportid")
-constructor(private val apiKey: String, private val listId: String, private val abuseReportId: String) {
-
-//    inline fun <reified T : Any> mock(): T = mock(T::class.java)
-
-//    var json = "{\"id\": 42,\"campaign_id\": \"839488a60b\",\"list_id\": \"1a2df69511\",\"email_id\": \"62eeb292278cc15f5817cb78f7790b08\",\"email_address\": \"urist.mcvankab@freddiesjokes.com\",\"date\": \"2015-07-15T19:19:31+00:00\",\"links\": [{\"rel\": \"self\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports/42\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Instance.json\" },{ \"rel\": \"parent\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Collection.json\",\"schema\": \"https://api.mailchimp.com/schema/3.0/CollectionLinks/Lists/Abuse.json\" }]}"
-//    var request = Connector.Request(HttpMethod.GET.name, "", "", "", "")
-//    var response = Connector.Response(200, "", json)
+@Parameters("mailchimp.test.apikey", "mailchimp.test.listid")
+constructor(private val apiKey: String, private val listId: String) {
+    private val log = Logger.getLogger(javaClass.getName())
 
     @Test
     fun testGetAbuseReports() {
@@ -28,27 +26,31 @@ constructor(private val apiKey: String, private val listId: String, private val 
         }
     }
 
-//    @Test
-//    fun testGetAbuseReport() {
-//
-//        var connector = HttpClientConnector()
-//        var spiedConnector = spy(connector)
-//
-//        var callMock = mock<HttpClientConnector>()
-//        `when`(spiedConnector.call()).thenReturn(response)
-//
-//        MailchimpClient(apiKey).use { client ->
-//            client.execute(GetAbuseReportMethod(listId, abuseReportId)).apply {
-//                Assert.assertNotNull(id)
-//                Assert.assertNotNull(campaign_id)
-//                Assert.assertNotNull(listId)
-//                Assert.assertNotNull(email_id)
-//                Assert.assertNotNull(email_address)
+    @Test
+    fun testGetAbuseReport() {
+        class MockMailchimpClient : MailchimpClient(apiKey, object : Connector {
+            override fun call(request: Connector.Request): Connector.Response {
+                val json = "{\"id\": 42,\"campaign_id\": \"839488a60b\",\"list_id\": \"1a2df69511\",\"email_id\": \"62eeb292278cc15f5817cb78f7790b08\",\"email_address\": \"urist.mcvankab@freddiesjokes.com\",\"date\": \"2015-07-15T19:19:31+00:00\",\"links\": [{\"rel\": \"self\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports/42\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Instance.json\" },{ \"rel\": \"parent\",\"href\": \"https://usX.api.mailchimp.com/3.0/lists/1a2df69511/abuse-reports\",\"method\": \"GET\",\"targetSchema\": \"https://api.mailchimp.com/schema/3.0/Lists/Abuse/Collection.json\",\"schema\": \"https://api.mailchimp.com/schema/3.0/CollectionLinks/Lists/Abuse.json\" }]}"
+                return Connector.Response(200, "", json)
+            }
+
+            override fun close() {
+            }
+        })
+
+        //TODO Improve test
+        MockMailchimpClient().use { client ->
+            client.execute(GetAbuseReportMethod(listId, "42")).apply {
+                Assert.assertNotNull(id)
+                Assert.assertNotNull(campaign_id)
+                Assert.assertNotNull(list_id)
+                Assert.assertNotNull(email_id)
+                Assert.assertNotNull(email_address)
 //                Assert.assertNotNull(merge_fields)
 //                Assert.assertNotNull(vip)
-//                Assert.assertNotNull(date)
+                Assert.assertNotNull(date)
 //                Assert.assertNotNull(_links)
-//            }
-//        }
-//    }
+            }
+        }
+    }
 }
